5116dacbed1be61c3afa58a5c9fa5b228932a2aa
Dream catcher <1428714197@qq.com>
2022-07-05 17:36:12 +0800

    添加图像识别技术

---
+++ TestFramework/src/test/java/framework/SearchTest.java
@@ -0,0 +1,73 @@
+package framework;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.io.File;
+import java.io.IOException;
+import java.time.Duration;
+import java.util.stream.Stream;
+
+
+/**
+ * @program:
+ * @description:
+ * @author: Mr.libb
+ * @creat: 2022-07-04 18-15
+ **/
+public class SearchTest {
+    static WebDriver driver;
+
+    @BeforeAll
+    public static void beforeAll() {
+        driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+    }
+
+    @AfterAll
+    public static void afterAll() {
+        driver.quit();
+    }
+
+
+    @ParameterizedTest
+    @MethodSource("data")
+    public void runTestCase(TestCase testCase) {
+        /**
+         * 统一的步骤处理
+         */
+      /*  driver.get("https://ceshiren.com");
+        driver.findElement(By.id("search-button")).click();
+
+        String keywords = "内推 BAT";
+        driver.findElement(By.id("search-term")).sendKeys(keywords+Keys.ENTER);
+        String title=driver.findElement(By.cssSelector(".topic-title")).get(0).getText();
+        assertThat(title,containsString(keywords));*/
+
+        testCase.run();
+    }
+
+    static Stream<TestCase> data() throws IOException {
+        /**
+         * 统一的数据读取
+         */
+       /* return Stream.of("src/test/java/framework/search.yaml",
+                "src/test/java/framework/search.yaml");
+        //todo: read from directory
+
+    }*/
+
+        ObjectMapper mapper = new ObjectMapper(new YAMLFactory());
+        TestCase testCase = mapper.readValue(new File("src/test/java/framework/search.yaml"),
+                TestCase.class);
+        return Stream.of(testCase);
+
+    }
+}
